76c7e56239bc162fe76bbd7834537ee4e9db6807
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-10-05 18:34:07 -0300

    feat: update Veiculo model class with Lombok annotations and field adjustments

--- backend/src/main/java/com/example/demo/model/Veiculo.java
+++ backend/src/main/java/com/example/demo/model/Veiculo.java
@@ -1,13 +1,22 @@
 package com.example.demo.model;
 // teste para commit
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Veiculo {
+
+    private Long id;
 
     private String placa;
     private String modelo;
     private String cor;
 
-    private boolean status;
-
-    
+    private boolean situacao;
+        
 }
 
