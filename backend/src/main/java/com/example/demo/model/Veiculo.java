70a2b0d4f857109ebf1c8c072cbe7b2553e940dc
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-10-05 10:18:58 -0300

    feat: add fields to Veiculo model class

--- backend/src/main/java/com/example/demo/model/Veiculo.java
+++ backend/src/main/java/com/example/demo/model/Veiculo.java
@@ -1,5 +1,13 @@
 package com.example.demo.model;
 // teste para commit
 public class Veiculo {
+
+    private String placa;
+    private String modelo;
+    private String cor;
+
+    private boolean status;
+
     
 }
+
