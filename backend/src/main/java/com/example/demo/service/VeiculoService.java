54be08453c1a7d1223a77541d2af5eb4758e89bc
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-10-05 19:56:12 -0300

    feat: initialize VeiculoService with a list of vehicles

--- backend/src/main/java/com/example/demo/service/VeiculoService.java
+++ backend/src/main/java/com/example/demo/service/VeiculoService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Veiculo;
+
+@Service
 public class VeiculoService {
+
+    private List<Veiculo> veiculos= new ArrayList<>();
+
+    public VeiculoService(){
+        veiculos.add(new Veiculo(1L, "CRI-2578", "Vectra", "LAranja",true));
+    }
     
 }
