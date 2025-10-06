a35c971907ad41b418ce2fb533529a693119203c
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-10-05 22:35:00 -0300

    refactor: clean up imports and update vehicle initialization in VeiculoService

--- backend/src/main/java/com/example/demo/service/VeiculoService.java
+++ backend/src/main/java/com/example/demo/service/VeiculoService.java
@@ -1,19 +1,19 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.example.demo.model.Veiculo;
+import org.springframework.stereotype.Service;
+import java.util.*;
 
 @Service
 public class VeiculoService {
 
     private List<Veiculo> veiculos= new ArrayList<>();
 
-    public VeiculoService(){
-        veiculos.add(new Veiculo(1L, "CRI-2578", "Vectra", "LAranja",true));
+   public VeiculoService() {
+      veiculos.add(new Veiculo(3L,"GHI-7777", "Corolla", "Branco", true));
+}
+
+    public List<Veiculo> listarveiculos(){
+        return veiculos;
     }
-    
 }
