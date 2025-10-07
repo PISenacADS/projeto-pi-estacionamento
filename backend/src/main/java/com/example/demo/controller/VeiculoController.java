d8a10804feacce009ebb597661441e20cb8c7368
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-10-07 17:12:58 -0300

    feat: update VeiculoController and VeiculoService for vehicle management; modify application properties for database connection

--- backend/src/main/java/com/example/demo/controller/VeiculoController.java
+++ backend/src/main/java/com/example/demo/controller/VeiculoController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Veiculo;
+import com.example.demo.service.VeiculoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/veiculos")
 public class VeiculoController {
-    
+
+    private final VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService) {
+        this.veiculoService = veiculoService;
+    }
+
+    @GetMapping
+    public List<Veiculo> listarVeiculos() {
+        return veiculoService.listarVeiculos();
+    }
 }
--- backend/src/main/java/com/example/demo/service/VeiculoService.java
+++ backend/src/main/java/com/example/demo/service/VeiculoService.java
@@ -10,9 +10,12 @@ public class VeiculoService {
 
     private List<Veiculo> veiculos = new ArrayList<>();
 
-    public VeiculoService() {
-        veiculos.add(new Veiculo(3L, "GHI-7777", "Corolla", "Branco", true, null));
-    }
+   public VeiculoService() {
+    veiculos.add(new Veiculo(1L, "CRI-2578", "Vectra", "Laranja", true,null));
+    veiculos.add(new Veiculo(2L, "XYZ-1234", "Civic", "Preto", false,null));
+    veiculos.add(new Veiculo(3L, "GHI-7777", "Corolla", "Branco", true,null));
+}
+    
 
     public List<Veiculo> listarVeiculos() {
         return veiculos;
