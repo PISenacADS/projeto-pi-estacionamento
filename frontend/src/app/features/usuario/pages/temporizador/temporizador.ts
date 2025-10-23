c5d415cb6f10ae93d6f509f1214a4d168f597750
VitorioAug <vito.augustodias@gmail.com>
2025-10-22 23:34:09 -0300

    Implement code changes to enhance functionality and improve performance

--- frontend/src/app/features/usuario/pages/temporizador/temporizador.ts
+++ frontend/src/app/features/usuario/pages/temporizador/temporizador.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+
+interface Veiculo {
+  placa: string;
+  tempo: string;
+  dataHora: string;
+  autorizacao: string;
+  localidade: string;
+}
 
 @Component({
   selector: 'app-temporizador',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './temporizador.html',
-  styleUrl: './temporizador.scss'
+  styleUrls: ['./temporizador.scss']
 })
-export class TemporizadorComponent {
+export class TemporizadorComponent implements OnInit {
 
+  public tempoExibido: string = "00:00";
+  public textoAte: string = "Até --:--";
+  public textoBotao: string = "PROLONGAR +";
+
+  public detalhesVeiculo!: Veiculo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.tempoExibido = "13:11";
+    this.textoAte = "Até 14:00";
+
+    this.detalhesVeiculo = {
+      placa: 'CRM - 5678',
+      tempo: '1h',
+      dataHora: '31/08/2025 às 13:00',
+      autorizacao: 'BDFFG134SGRT',
+      localidade: 'SP (São Paulo)'
+    };
+  }
+
+  prolongarTempo(): void {
+    alert('Ação "PROLONGAR +" acionada!');
+    this.textoBotao = 'Tempo Estendido!';
+  }
 }
