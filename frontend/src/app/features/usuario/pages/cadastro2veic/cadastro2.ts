5baa0218f982520ad6cddd38de6596e6226f9dad
VitorioAug <vito.augustodias@gmail.com>
2025-11-11 00:46:28 -0300

    Refactor: Cadastro2

---
+++ frontend/src/app/features/usuario/pages/cadastro2veic/cadastro2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro2',
+  imports: [],
+  templateUrl: './cadastro2.html',
+  styleUrl: './cadastro2.scss'
+})
+export class cadastro2Component {
+
+}
