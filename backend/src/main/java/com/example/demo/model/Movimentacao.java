5b939463b15bbdfdf3ed10ff3c410490bcadbb4b
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-11-12 23:58:31 -0300

    Refactor code structure for improved readability and maintainability

--- backend/src/main/java/com/example/demo/model/Movimentacao.java
+++ backend/src/main/java/com/example/demo/model/Movimentacao.java
@@ -1,5 +1,20 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movimentacao {
-    
+    private Long id;               
+    private String placa;         
+    private LocalDateTime entrada; 
+    private LocalDateTime saida;  
+    private Double valor;    
+    private boolean ativa; 
 }
