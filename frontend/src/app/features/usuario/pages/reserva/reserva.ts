7eb3c49a32b8c4093b1d489874a461baf38cd216
VitorioAug <vito.augustodias@gmail.com>
2025-11-23 18:23:31 -0300

    Feat: Elaborado tela reserva

--- frontend/src/app/features/usuario/pages/reserva/reserva.ts
+++ frontend/src/app/features/usuario/pages/reserva/reserva.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-reserva',
   imports: [],
   templateUrl: './reserva.html',
   styleUrl: './reserva.scss'
+
 })
 export class ReservaComponent {
+
 
 }
